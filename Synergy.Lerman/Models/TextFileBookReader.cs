bc3221f662edb5beae93e92587bc802e1d989929
Marcin Celej <marcin.celej@gmail.com>
2017-08-16 15:10:19 +0200

    added irregular verbs book

--- Synergy.Lerman/Models/TextFileBookReader.cs
+++ Synergy.Lerman/Models/TextFileBookReader.cs
@@ -20,37 +20,59 @@ namespace Synergy.Lerman.Controllers
 
                 if (book == null)
                 {
-                    var bookName = ReadName(line);
-                    book = new Book(bookName);
+                    book = CreateBook(line);
                     continue;
                 }
 
                 if (line.StartsWith("["))
                 {
                     inverted = line.EndsWith("-");
-                    var name = ReadName(line);
-                    category = new Category(book, name);
-                    book.Categories.Add(category);
+                    category = CreateCategory(book, line);
                     continue;
                 }
 
-                var translations = line.Split('=');
-                Fail.IfFalse(translations.Length == 2, "Something wrong with line '{0}'", line);
-
-                string en = translations[0];
-                string pl = translations[1];
-
-                if (inverted)
-                {
-                    pl = translations[0];
-                    en = translations[1];
-                }
-
-                var word = new Word(en, pl, category);
-                category.Words.Add(word);
+                CreateWord(category, inverted, line);
             }
 
+            book.Categories.TrimExcess();
+            book.Categories.ForEach(c => c.Words.TrimExcess());
+
             return book;
+        }
+
+        private static Book CreateBook(string line)
+        {
+            Book book;
+            var bookName = ReadName(line);
+            book = new Book(bookName);
+            return book;
+        }
+
+        private static Category CreateCategory(Book book, string line)
+        {
+            Category category;
+            var name = ReadName(line);
+            category = new Category(book, name);
+            book.Categories.Add(category);
+            return category;
+        }
+
+        private static void CreateWord(Category category, bool inverted, string line)
+        {
+            var translations = line.Split('=');
+            Fail.IfFalse(translations.Length == 2, "Something wrong with line '{0}'", line);
+
+            string en = translations[0];
+            string pl = translations[1];
+
+            if (inverted)
+            {
+                pl = translations[0];
+                en = translations[1];
+            }
+
+            var word = new Word(en, pl, category);
+            category.Words.Add(word);
         }
 
         private static string ReadName(string line)
